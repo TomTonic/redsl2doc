ee8aad2c235c2519c143f66005cd37de0c45989e
Tom Tonic <tom@gelhausen.com>
2019-02-21 18:20:00 +0100

    added build scripts & co.

---
+++ testCompiler.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+TESTFILE="$SCRIPTPATH/Compiler/testdata/ex2.redsl"
+OUTPUTDIR="$SCRIPTPATH/Compiler/testdata/"
+cd "$SCRIPTPATH/Compiler/"
+dotnet run -- "$TESTFILE" -o "$OUTPUTDIR"
