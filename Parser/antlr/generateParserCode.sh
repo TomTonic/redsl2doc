ee83bcfd6880a851cc32bf51900da3c8c4996f8f
Tom Tonic <tom@tomtonic.de>
2025-05-30 12:50:08 +0200

    Update ANTLR version from 4.7.2 to 4.13.2 and adjust related scripts

--- Parser/antlr/generateParserCode.sh
+++ Parser/antlr/generateParserCode.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx500M -cp "../antlr-4.7.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool -o ../../Compiler/src/gen/ -package org.ReDSL.Parser -Dlanguage=CSharp ReDSLParser.g4 ReDSLLexer.g4
+java -Xmx500M -cp "../antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool -o ../../Compiler/src/gen/ -package org.ReDSL.Parser -Dlanguage=CSharp ReDSLParser.g4 ReDSLLexer.g4
--- buildParser.sh
+++ buildParser.sh
@@ -4,4 +4,4 @@ TARGETPATH="$SCRIPTPATH/Compiler/src/gen/"
 SOURCEPATH="$SCRIPTPATH/Parser/antlr/"
 rm -rf "$TARGETPATH/*"
 cd "$SOURCEPATH"
-java -Xmx500M -cp "../antlr-4.7.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool -o "$TARGETPATH" -package org.ReDSL.Parser -Dlanguage=CSharp ReDSLParser.g4 ReDSLLexer.g4
+java -Xmx500M -cp "../antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool -o "$TARGETPATH" -package org.ReDSL.Parser -Dlanguage=CSharp ReDSLParser.g4 ReDSLLexer.g4
