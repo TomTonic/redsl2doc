74da87cb29f50f0b62f0e09194d30476e9314126
Tom Tonic <tom@tomtonic.de>
2025-05-30 22:26:32 +0200

    Improve error handling and incorporate linter recommendations

--- Compiler/src/DocGen.cs
+++ Compiler/src/DocGen.cs
@@ -45,8 +45,7 @@ namespace org.redsl.Compiler
 
         public static string ToRoman(int number)
         {
-            if ((number < 0) || (number > 3999)) throw new ArgumentOutOfRangeException("insert value betwheen 1 and 3999");
-            if (number < 1) return string.Empty;
+            if ((number < 0) || (number > 3999)) throw new ArgumentOutOfRangeException("number parameter value was "+number+", must be between 1 and 3999 (inclusively)");
             if (number >= 1000) return "M" + ToRoman(number - 1000);
             if (number >= 900) return "CM" + ToRoman(number - 900);
             if (number >= 500) return "D" + ToRoman(number - 500);
@@ -60,7 +59,8 @@ namespace org.redsl.Compiler
             if (number >= 5) return "V" + ToRoman(number - 5);
             if (number >= 4) return "IV" + ToRoman(number - 4);
             if (number >= 1) return "I" + ToRoman(number - 1);
-            throw new ArgumentOutOfRangeException("something bad happened");
+            // number must be <1
+            return string.Empty;
         }
 
         public void GenerateDocuments(XDocument doc, string basefilename)
@@ -88,38 +88,36 @@ namespace org.redsl.Compiler
             string fullfilename = BuildFileName(basefilename, documentTitle);
             PackageNumber = 0;
 
-            using (WordprocessingDocument myDocument =
-                            WordprocessingDocument.Create(fullfilename, WordprocessingDocumentType.Document))
+            using WordprocessingDocument myDocument =
+                            WordprocessingDocument.Create(fullfilename, WordprocessingDocumentType.Document);
+            // Add a main part
+            MainDocumentPart mainPart = myDocument.AddMainDocumentPart();
+            AddStyles(myDocument);
+
+            // Create the document structure
+            mainPart.Document = new Document();
+            Body body = mainPart.Document.AppendChild(new Body());
+
+            Paragraph p = AppendParagraph(body, documentTitle);
+            SetStype(p, "DocumentTitle");
+
+            var docdeclNodes =
+                from AnyElement in docDecl.Descendants()
+                where (
+                    (AnyElement.NodeType == XmlNodeType.Element)
+                    && (((XElement)AnyElement).Name.Equals(XName.Get("PackageRef")))
+                    )
+                select AnyElement;
+
+            foreach (XElement packageRef in docdeclNodes)
             {
-                // Add a main part.   
-                MainDocumentPart mainPart = myDocument.AddMainDocumentPart();
-                AddStyles(myDocument);
-
-                // Create the document structure.  
-                mainPart.Document = new Document();
-                Body body = mainPart.Document.AppendChild(new Body());
-
-                Paragraph p = appendParagraph(body, documentTitle);
-                setStype(p, "DocumentTitle");
-
-                var docdeclNodes =
-                    from AnyElement in docDecl.Descendants()
-                    where (
-                        (AnyElement.NodeType == XmlNodeType.Element)
-                        && (((XElement)AnyElement).Name.Equals(XName.Get("PackageRef")))
-                        )
-                    select AnyElement;
-
-                foreach (XElement packageRef in docdeclNodes)
-                {
-                    AddPackage(body, packageRef);
-                }
-
-                MarkAsVisited(docDecl);
+                AddPackage(body, packageRef);
             }
+
+            MarkAsVisited(docDecl);
         }
 
-        private string BuildFileName(string basefilename, string documentTitle)
+        private static string BuildFileName(string basefilename, string documentTitle)
         {
             if (!Directory.Exists(basefilename))
             {
@@ -130,24 +128,21 @@ namespace org.redsl.Compiler
             return result;
         }
 
-        private void AddStyles(WordprocessingDocument doc)
+        private static void AddStyles(WordprocessingDocument doc)
         {
             // Get the Styles part for this document.
             StyleDefinitionsPart styleDefPart =
                 doc.MainDocumentPart.StyleDefinitionsPart;
 
             // If the Styles part does not exist, add it and then add the style.
-            if (styleDefPart == null)
-            {
-                styleDefPart = AddStylesPartToPackage(doc);
-            }
+            styleDefPart ??= AddStylesPartToPackage(doc);
             CreateAndAddParagraphStyle(styleDefPart, "DocumentTitle", "Document Title", new FontSize() { Val = "64" }, new Bold());
             CreateAndAddParagraphStyle(styleDefPart, "PackageTitle", "Package Title", new FontSize() { Val = "32" }, new Bold());
             CreateAndAddParagraphStyle(styleDefPart, "NoContent", "No-Content Paragraphs", new Italic());
             CreateAndAddParagraphStyle(styleDefPart, "RecDecl", "Requirement");
         }
 
-        private void MarkAsVisited(XElement node)
+        private static void MarkAsVisited(XElement node)
         {
             node.SetAttributeValue("visited", "true");
         }
@@ -158,9 +153,8 @@ namespace org.redsl.Compiler
             RelativeReqNumber = 0;
             string myPackageNumber = GetPackageNumberString();
             string packageName = packageRef.Attribute("ID_STR").Value;
-            Paragraph title = appendParagraph(body, myPackageNumber + "\t" + packageName);
-            setStype(title, "PackageTitle");
-
+            Paragraph title = AppendParagraph(body, myPackageNumber + "\t" + packageName);
+            SetStype(title, "PackageTitle");
 
             XElement root = packageRef.Document.Root;
             var packageNodes =
@@ -172,11 +166,11 @@ namespace org.redsl.Compiler
                     )
                 select AnyElement;
 
-            List<XElement> reqDeclNodes = new List<XElement>();
+            List<XElement> reqDeclNodes = [];
             foreach (XElement packageNode in packageNodes)
             {
                 var children = packageNode.DescendantNodes();
-                foreach (XElement child in children)
+                foreach (XElement child in children.Cast<XElement>())
                 {
                     if (child.Name.Equals(XName.Get("RequirementDecl")))
                     {
@@ -187,8 +181,8 @@ namespace org.redsl.Compiler
             }
             if (reqDeclNodes.Count == 0)
             {
-                Paragraph p = appendParagraph(body, "No content.");
-                setStype(p, "NoContent");
+                Paragraph p = AppendParagraph(body, "No content.");
+                SetStype(p, "NoContent");
             }
             else
             {
@@ -207,17 +201,17 @@ namespace org.redsl.Compiler
             RelativeReqNumber++;
             string reqIDLabel = GetRelativeReqNumberString();
             string plainText = GetPlainTextFromReqDecl(reqDeclNode);
-            Paragraph p = appendParagraph(body, reqIDLabel + "\t" + plainText);
+            Paragraph p = AppendParagraph(body, reqIDLabel + "\t" + plainText);
             //append(p, plainText);
-            setStype(p, "RecDecl");
+            SetStype(p, "RecDecl");
         }
 
-        private void append(Paragraph p, string plainText)
+        private static void Append(Paragraph p, string plainText)
         {
             p.AppendChild(new Run(new Text(plainText) { Space = SpaceProcessingModeValues.Preserve }));
         }
 
-        private string GetPlainTextFromReqDecl(XElement reqDeclNode)
+        private static string GetPlainTextFromReqDecl(XElement reqDeclNode)
         {
             var textBlocks =
                     from AnyElement in reqDeclNode.Descendants()
@@ -238,17 +232,17 @@ namespace org.redsl.Compiler
             return result;
         }
 
-        private Paragraph appendParagraph(Body body, string text)
+        private static Paragraph AppendParagraph(Body body, string text)
         {
-            Paragraph p = new Paragraph(new Run(new Text(text) { Space = SpaceProcessingModeValues.Preserve }));
+            Paragraph p = new(new Run(new Text(text) { Space = SpaceProcessingModeValues.Preserve }));
             body.AppendChild(p);
             return p;
         }
 
-        private void setStype(Paragraph p, string styleid)
+        private static void SetStype(Paragraph p, string styleid)
         {
             // If the paragraph has no ParagraphProperties object, create one.
-            if (p.Elements<ParagraphProperties>().Count() == 0)
+            if (!p.Elements<ParagraphProperties>().Any())
             {
                 p.PrependChild<ParagraphProperties>(new ParagraphProperties());
             }
@@ -260,14 +254,13 @@ namespace org.redsl.Compiler
             pPr.Indentation = new Indentation() { FirstLine = "0", Hanging = "750" };
 
             // If a ParagraphStyleId object does not exist, create one.
-            if (pPr.ParagraphStyleId == null)
-                pPr.ParagraphStyleId = new ParagraphStyleId();
+            pPr.ParagraphStyleId ??= new ParagraphStyleId();
 
             // Set the style of the paragraph.
             pPr.ParagraphStyleId.Val = styleid;
         }
 
-        public void CreateAndAddParagraphStyle(StyleDefinitionsPart styleDefinitionsPart,
+        public static void CreateAndAddParagraphStyle(StyleDefinitionsPart styleDefinitionsPart,
            string styleid, string stylename, params OpenXmlElement[] styleElements)
         {
             // Access the root element of the styles part.
@@ -279,7 +272,7 @@ namespace org.redsl.Compiler
             }
 
             // Create a new paragraph style element and specify some of the attributes.
-            Style style = new Style()
+            Style style = new()
             {
                 Type = StyleValues.Paragraph,
                 StyleId = styleid,
@@ -288,16 +281,16 @@ namespace org.redsl.Compiler
             };
 
             // Create and add the child elements (properties of the style).
-            AutoRedefine autoredefine1 = new AutoRedefine() { Val = OnOffOnlyValues.Off };
+            AutoRedefine autoredefine1 = new() { Val = OnOffOnlyValues.Off };
             //BasedOn basedon1 = new BasedOn() { Val = basedOn };
-            Locked locked1 = new Locked() { Val = OnOffOnlyValues.Off };
-            PrimaryStyle primarystyle1 = new PrimaryStyle() { Val = OnOffOnlyValues.On };
-            StyleHidden stylehidden1 = new StyleHidden() { Val = OnOffOnlyValues.Off };
-            SemiHidden semihidden1 = new SemiHidden() { Val = OnOffOnlyValues.Off };
-            StyleName styleName1 = new StyleName() { Val = stylename };
-            NextParagraphStyle nextParagraphStyle1 = new NextParagraphStyle() { Val = "Normal" };
-            UIPriority uipriority1 = new UIPriority() { Val = 1 };
-            UnhideWhenUsed unhidewhenused1 = new UnhideWhenUsed() { Val = OnOffOnlyValues.On };
+            Locked locked1 = new() { Val = OnOffOnlyValues.Off };
+            PrimaryStyle primarystyle1 = new() { Val = OnOffOnlyValues.On };
+            StyleHidden stylehidden1 = new() { Val = OnOffOnlyValues.Off };
+            SemiHidden semihidden1 = new() { Val = OnOffOnlyValues.Off };
+            StyleName styleName1 = new() { Val = stylename };
+            NextParagraphStyle nextParagraphStyle1 = new() { Val = "Normal" };
+            UIPriority uipriority1 = new() { Val = 1 };
+            UnhideWhenUsed unhidewhenused1 = new() { Val = OnOffOnlyValues.On };
 
             style.Append(autoredefine1);
             //style.Append(basedon1);
@@ -311,19 +304,7 @@ namespace org.redsl.Compiler
             style.Append(unhidewhenused1);
 
             // Create the StyleRunProperties object and specify some of the run properties.
-            StyleRunProperties styleRunProperties = new StyleRunProperties();
-            // Bold bold1 = new Bold();
-            // Color color1 = new Color() { ThemeColor = ThemeColorValues.Accent2 };
-            // RunFonts font1 = new RunFonts() { Ascii = "Lucida Console" };
-            // Italic italic1 = new Italic();
-            // // Specify a 12 point size.
-            // FontSize fontSize1 = new FontSize() { Val = "24" };
-            // styleRunProperties1.Append(bold1);
-            // styleRunProperties1.Append(color1);
-            // styleRunProperties1.Append(font1);
-            // styleRunProperties1.Append(fontSize1);
-            // styleRunProperties1.Append(italic1);
-
+            StyleRunProperties styleRunProperties = new();
             foreach (var styleElement in styleElements)
             {
                 styleRunProperties.Append(styleElement);
@@ -337,11 +318,11 @@ namespace org.redsl.Compiler
         }
 
         // Add a StylesDefinitionsPart to the document.  Returns a reference to it.
-        public StyleDefinitionsPart AddStylesPartToPackage(WordprocessingDocument doc)
+        public static StyleDefinitionsPart AddStylesPartToPackage(WordprocessingDocument doc)
         {
             StyleDefinitionsPart part;
             part = doc.MainDocumentPart.AddNewPart<StyleDefinitionsPart>();
-            Styles root = new Styles();
+            Styles root = new();
             root.Save(part);
             return part;
         }
