e225cdb6501ee95cce245c585e7839e56caebeee
Tom Tonic <tom@tomtonic.de>
2025-09-03 14:51:27 +0200

    Upload artefacts after build

--- packCompiler.sh
+++ packCompiler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$SCRIPTPATH/Compiler/"
-dotnet publish -c Release -r osx-x64 --self-contained true
-dotnet publish -c Release -r win-x64 --self-contained true
-dotnet publish -c Release -r linux-x64 --self-contained true
+dotnet publish -c Release -r osx-arm64 --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true
+dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true
