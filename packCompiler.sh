699cc99e7cd41a160dfa07159889358c1ee8719e
Tom Gelhausen <tom.gelhausen@capgemini.com>
2025-09-03 11:02:04 +0200

    Add GitHub Actions workflow for building compiler and scripts for parser generation

--- packCompiler.sh
+++ packCompiler.sh
@@ -2,5 +2,5 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$SCRIPTPATH/Compiler/"
 dotnet publish -c Release -r osx-x64 --self-contained true
-dotnet publish -c Release -r win10-x64 --self-contained true
+dotnet publish -c Release -r win-x64 --self-contained true
 dotnet publish -c Release -r linux-x64 --self-contained true
