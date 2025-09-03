2570b248cce1c3b6c2f53b088aaea30b23ee87e8
Tom Tonic <tom@tomtonic.de>
2025-09-03 11:02:40 +0200

    Remove duplicate script

--- buildParser.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-TARGETPATH="$SCRIPTPATH/Compiler/src/gen/"
-SOURCEPATH="$SCRIPTPATH/Parser/antlr/"
-rm -rf "$TARGETPATH/*"
-cd "$SOURCEPATH"
-java -Xmx500M -cp "../antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool -o "$TARGETPATH" -package org.ReDSL.Parser -Dlanguage=CSharp ReDSLParser.g4 ReDSLLexer.g4
