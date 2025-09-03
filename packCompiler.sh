bd88d77c2902271baa2ec38ed0a80410745f83ed
Tom Tonic <tom@tomtonic.de>
2025-09-03 15:23:30 +0200

    Omit vm and libraries in packaging to reduce file size
    Signed-off-by: Tom Tonic <tom@tomtonic.de>

--- packCompiler.sh
+++ packCompiler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$SCRIPTPATH/Compiler/"
-dotnet publish -c Release -r osx-arm64 --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true
-dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true
-dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true /p:PublishTrimmed=true
+dotnet publish -c Release -r osx-arm64 --self-contained true /p:PublishSingleFile=true /p:SelfContained=false /p:PublishTrimmed=false
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishSingleFile=true /p:SelfContained=false /p:PublishTrimmed=false
+dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true /p:SelfContained=false /p:PublishTrimmed=false
